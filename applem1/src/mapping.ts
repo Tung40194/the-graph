4f1295f646da8fe1e50a47a9c35fa1a0e3b77918
tungpham <tungpham@lecle.co.kr>
2022-05-18 15:42:21 +0700

    update applem1 subgraph with subgraph description that help the graph catch contract deployment event

---
+++ applem1/src/mapping.ts
@@ -0,0 +1,29 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  manOfBinary,
+  createContract
+} from "../generated/manOfBinary/manOfBinary"
+import { contractDeployment } from "../generated/schema"
+
+export function handlecreateContract(event: createContract): void {
+  let entity = contractDeployment.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new contractDeployment(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.name = event.params.name
+  entity.symbol = event.params.symbol
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+}
